9ff680570c13719a4064aed4a41f0ac59f9eb53e
SunnY <93282477+nakerin7588@users.noreply.github.com>
2023-03-03 11:34:36 +0700

    Update Person_main.cpp

--- lab_program_fra/LAB4/Person_main.cpp
+++ lab_program_fra/LAB4/Person_main.cpp
@@ -8,21 +8,24 @@ int main()
 {
 
     // add code here
-    // Person firstperson("Sun",180,19);
+    // Person firstperson;
+    // firstperson.name = "Sun";
+    // firstperson.age = 19;
+    // firstperson.height = 100;
     // firstperson.showPersonInfo();
     // cout << endl;
-    // Student firststudent("EVE",200,5,73,10);
+    // Student firststudent("EVE", 200, 5, 73, 10);
     // firststudent.showStudenInfo();
-    // Teacher firstteacher("Blink",300,100,"Programming");
+    // Teacher firstteacher("Fibo", 300, 100, "Programming");
     // cout << endl;
     // firstteacher.showTeacherInfo();
     // firstteacher.setSubject("Mechanic");
     // cout << endl;
     // cout << firstteacher.getSubject() << endl;
 
-    Node q = Node(1, NULL, NULL);
-    Node w = Node(2, NULL, NULL);
-    Node e = Node(3, NULL, NULL);
+    // Node q = Node(1, NULL, NULL);
+    // Node w = Node(2, NULL, NULL);
+    // Node e = Node(3, NULL, NULL);
 
     // Stack a;
     // a.push(&q);
@@ -32,17 +35,62 @@ int main()
     // a.push(&e);
     // cout << a.peek() << endl;
     // cout << endl;
-    // cout << a.pop() << endl;
-    // cout << a.pop() << endl;
-    // cout << a.pop() << endl;
+    // cout << "POP:" << a.pop() << endl;
+    // cout << a.peek() << endl;
+    // cout << "POP:" << a.pop() << endl;
+    // cout << a.peek() << endl;
+    // cout << "POP:" << a.pop() << endl;
+    // cout << a.peek() << endl;
+    // cout << endl;
 
-    Queue b;
-    b.enqueue(&q);
-    b.enqueue(&w);
-    b.enqueue(&e);
-    cout << b.peek() << endl;
-    cout << b.dequeue() << endl;
-    cout << b.dequeue() << endl;
-    cout << b.dequeue() << endl;
+    // Queue b;
+    // b.enqueue(&q);
+    // // cout << b.peek() << endl;
+    // b.enqueue(&w);
+    // // cout << b.peek() << endl;
+    // b.enqueue(&e);
+    // // cout << b.peek() << endl;
+    // cout << endl;
+    // cout << "Dequeue:" << b.dequeue() << endl;
+    // cout << b.peek() << endl;
+    // cout << "Dequeue:" << b.dequeue() << endl;
+    // cout << b.peek() << endl;
+    // cout << "Dequeue:" << b.dequeue() << endl;
+    // cout << b.peek() << endl;
+
+    // post lab
+
+    Stack stack_card;
+    // int card_array[9] = {1,5,0,3,9,4,2,7,6};
+    int card_array_int[6] = {1, 2, 3, 0, 4, 5};
+    Node card_array_int_node[6];
+    for (int i = 0; i < size(card_array_int); i++)
+    {
+        card_array_int_node[i] = Node(card_array_int[i],NULL,NULL);
+    }
+    // for (int i = 0; i < size(card_array_int); i++)
+    // {
+    //     cout << card_array_int_node[i].getValue() << endl; 
+    // }
+    
+    // Node card_array[6] = {Node(1, NULL, NULL), Node(2, NULL, NULL), Node(3, NULL, NULL), Node(0, NULL, NULL), Node(4, NULL, NULL), Node(5, NULL, NULL)};
+    for (int i = 0; i < size(card_array_int_node); i++)
+    {
+        if (card_array_int_node[i].getValue() == 0)
+        {
+            Node *temp = stack_card.peek();
+            stack_card.pop();
+            stack_card.push(&card_array_int_node[i]);
+            stack_card.push(temp);
+        }
+        else
+        {
+            stack_card.push(&card_array_int_node[i]);
+        }
+    }
+    for (int i = 0; i < size(card_array_int_node); i++)
+    {
+        cout << stack_card.pop() << endl;
+    }
     return 0;
 }
